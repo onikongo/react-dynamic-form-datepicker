7b686c4d219eb66d66a9b9280b29c73163503aea
Dexter Marks-Barber <dexter.mb@outlook.com>
2018-11-23 10:39:24 +0000

    Initial commit

---
+++ src/datepicker.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Util from '@ninetynine/util';
+
+const FormDatePicker = ({ name, placeholder, value, valid, onChange }) => {
+    const onInternalChange = date => {
+        const event = document.createEvent('Event');
+        const value = date.toISOString()
+            .slice(0, 19)
+            .replace('T', ' ');
+
+        event.initEvent('onChange', true, true);
+        window.dispatchEvent(event);
+
+        Object.defineProperty(event, 'target', {
+            writable: false,
+            value: {
+                value,
+                name,
+            },
+        });
+
+        Object.defineProperty(event, 'persist', {
+            writable: false,
+            value: () => null,
+        });
+
+        onChange(event);
+    };
+
+    const className = classNames([
+        'rdf-input',
+        { 'input-invalid': !!valid },
+    ]);
+
+    return (
+        <DatePicker
+            className={className}
+            placeholderText={placeholder || 'Select a date'}
+            selected={Util.isEmpty(value) ? null : new Date(value)}
+            onChange={onInternalChange}
+        />
+    );
+};
+
+FormDatePicker.defaultProps = {
+    name: 'date',
+    placeholder: 'Select a date',
+    value: null,
+    valid: true,
+    onChange: () => null,
+};
+
+FormDatePicker.propTypes = {
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    valid: PropTypes.bool,
+    onChange: PropTypes.func,
+};
+
+export { FormDatePicker as default };
